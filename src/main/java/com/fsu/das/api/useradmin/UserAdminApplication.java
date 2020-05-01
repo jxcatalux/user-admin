ce99889193075501f4cc97dab434034127380a1d
jxcatalux <52547832+jxcatalux@users.noreply.github.com>
2020-05-01 09:43:00 -0400

    Add files via upload

---
+++ src/main/java/com/fsu/das/api/useradmin/UserAdminApplication.java
@@ -0,0 +1,28 @@
+package com.fsu.das.api.useradmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class UserAdminApplication {
+
+	@GetMapping("/")
+	public String welcome() {
+		return "Welcome to OpenShift world!";
+		
+	}
+	
+	@GetMapping("/{input}")
+	public String congrats(@PathVariable String input) {
+		return "hi " + input + " Your application deployed successfully....";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(UserAdminApplication.class, args);
+	}
+
+}
